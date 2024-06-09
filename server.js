fc91197ca44c94fcdf2e101320688b14cbc4794c
Kunamidev <kunnami661@gmail.com>
2024-06-09 23:46:13 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/send-sms', async (req, res) => {
+    const { number, amount, delay } = req.query;
+    try {
+        const response = await axios.get(`https://deku-rest-api-3ijr.onrender.com/smsb`, {
+            params: { number, amount, delay }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('Error sending SMS');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
